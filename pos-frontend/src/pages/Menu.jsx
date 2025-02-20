cb4f68926261805f0c9c3398ef4b286116e206dc
Azeem Asim <azeem.asim01@gmail.com>
2025-02-20 22:30:51 +0500

    enhance Menu page layout with customer order details and action buttons

--- pos-frontend/src/pages/Menu.jsx
+++ pos-frontend/src/pages/Menu.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import BackButton from "../components/shared/BackButton";
 import { MdRestaurantMenu } from "react-icons/md";
+import { RiDeleteBin2Fill } from "react-icons/ri";
+import { FaNotesMedical } from "react-icons/fa";
 import MenuContainer from "../components/menu/MenuContainer";
 
 const Menu = () => {
@@ -25,8 +27,40 @@ const Menu = () => {
           <MenuContainer />
         </div>
         {/* right */}
-        <div className="flex-1 bg-amber-500">
-          <h1>Right</h1>
+        <div className="flex-1 bg-[#2a2a2a] h-screen">
+          <div className="flex justify-between items-start p-4">
+            <div>
+              <h1 className="text-xl">Customer Name</h1>
+              <p className="text-sm mt-2 text-[#ababab]">#101/Dine In</p>
+              <p className="text-sm mt-2 text-[#ababab]">
+                January 19, 2025 05:35 PM
+              </p>
+            </div>
+            <div>
+              <button className="bg-[#F6B100] text-xl p-3 font-bold rounded-lg text-[#1f1f1f]">
+                CN
+              </button>
+            </div>
+          </div>
+          <hr className="border-[#3a3a3a] border-t-2 " />
+          <div className="px-4 py-2">
+          <h1 className="text-lg">Order Details</h1>
+          </div>
+          <div className="px-4 py-2 text-[#ababab]">
+            <div className="bg-[#1f1f1f] p-2">
+              <div className="flex justify-between items-center">
+                <h1 className="text-lg font-semibold">Chicken Tikka</h1>
+                <p>x2</p>
+              </div>
+              <div className="flex justify-between mt-2">
+                <div className="flex gap-2">
+                  <RiDeleteBin2Fill className="cursor-pointer" size={20} />
+                  <FaNotesMedical className="cursor-pointer" size={20} />
+                </div>
+                <p className="font-semibold">123 Rs.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
