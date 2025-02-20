7af0a513a68491d70185215cb132bf4e6fe163b8
Azeem Asim <azeem.asim01@gmail.com>
2025-02-20 17:12:46 +0500

    add item count functionality to MenuContainer; implement increment and decrement buttons for menu items

--- pos-frontend/src/components/menu/MenuContainer.jsx
+++ pos-frontend/src/components/menu/MenuContainer.jsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react'
-import { menus } from '../../constants/'
-import { GrRadialSelected } from 'react-icons/gr'
-import { getBgColor } from '../../utils';
+import React, { useState } from "react";
+import { menus } from "../../constants/";
+import { GrRadialSelected } from "react-icons/gr";
+import { getBgColor } from "../../utils";
 
 const MenuContainer = () => {
   const [selected, setSelected] = useState(menus[0]);
+  const [itemsCount, setItemsCount] = useState(0);
+  const [itemId, setItemId] = useState(0);
+
+  const increment = (id) => {
+    setItemId(id);
+    if (itemsCount >= 6) return;
+    setItemsCount((prev) => prev + 1);
+  };
+  const decrement = (id) => {
+    setItemId(id);
+    if (itemsCount <= 0) return;
+    setItemsCount((prev) => prev - 1);
+  };
 
   return (
     <>
@@ -13,7 +26,11 @@ const MenuContainer = () => {
           <div
             key={menu.id}
             className={`${getBgColor()} p-4 rounded-md`}
-            onClick={() => setSelected(menu)}
+            onClick={() => {
+              setSelected(menu)
+              setItemId(0)
+              setItemsCount(0)
+            }}
           >
             <div className="flex justify-between items-center">
               <h1 className="text-xl font-semibold">
@@ -40,14 +57,31 @@ const MenuContainer = () => {
             <div className="flex justify-between items-center">
               <h1 className="text-xl font-semibold">{menu.name}</h1>
             </div>
-            <div className="text-sm [text-shadow:_1px_1px_2px_rgb(0_0_0_/_0.8)]">
-              {menu.price} Rs.
+            <div className="flex justify-between items-center">
+              <div className="text-sm [text-shadow:_1px_1px_2px_rgb(0_0_0_/_0.8)]">
+                {menu.price} Rs.
+              </div>
+              <div className="flex justify-between items-center rounded-lg gap-6 p-2 px-4 bg-[#1f1f1f]">
+                <button
+                  onClick={()=>decrement(menu.id)}
+                  className="text-yellow-400"
+                >
+                  &minus;
+                </button>
+                <span> {menu.id == itemId ? itemsCount : 0} </span>
+                <button
+                  onClick={()=>increment(menu.id)}
+                  className="text-yellow-400"
+                >
+                  &#43;
+                </button>
+              </div>
             </div>
           </div>
         ))}
-      </div>      
+      </div>
     </>
   );
-}
+};
 
-export default MenuContainer
+export default MenuContainer;
